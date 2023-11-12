30812fa1ff044b7853377229002b0f56dfa7b0b8
guiireal <guii_real@hotmail.com>
2023-11-12 01:40:48 -0300

    wip

--- src/commands/member/sticker.js
+++ src/commands/member/sticker.js
@@ -6,9 +6,9 @@ const { exec } = require("child_process");
 
 module.exports = {
   name: "sticker",
-  description: "Faço figurinhas de imagem/gif e vídeo",
+  description: "Faço figurinhas de imagem/gif/vídeo",
   commands: ["s", "sticker", "fig", "f"],
-  usage: `${PREFIX}sticker (marque a imagem/gif/video) ou ${PREFIX}sticker (responda a imagem/gif/video)`,
+  usage: `${PREFIX}sticker (marque a imagem/gif/vídeo) ou ${PREFIX}sticker (responda a imagem/gif/vídeo)`,
   handle: async ({
     isImage,
     isVideo,
@@ -21,7 +21,7 @@ module.exports = {
   }) => {
     if (!isImage && !isVideo) {
       throw new InvalidParameterError(
-        "Você precisa marcar uma imagem/gif/video ou responder a uma imagem/gif/video"
+        "Você precisa marcar uma imagem/gif/vídeo ou responder a uma imagem/gif/vídeo"
       );
     }
 
@@ -73,6 +73,7 @@ Envie um vídeo menor!`);
         `ffmpeg -i ${inputPath} -y -vcodec libwebp -fs 0.99M -filter_complex "[0:v] scale=512:512,fps=12,pad=512:512:-1:-1:color=white@0.0,split[a][b];[a]palettegen=reserve_transparent=on:transparency_color=ffffff[p];[b][p]paletteuse" -f webp ${outputPath}`,
         async (error) => {
           if (error) {
+            console.log(error);
             fs.unlinkSync(inputPath);
 
             throw new Error(error);
