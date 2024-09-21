dc9a6007cd9a8003b6905abc1becf8791d7f8d4b
guiireal <guii_real@hotmail.com>
2024-09-21 13:52:57 -0300

    wip

--- src/commands/_sample.js
+++ src/commands/_sample.js
@@ -6,6 +6,44 @@
  * Pasta owner: Comandos que só podem ser executados pelo dono do bot
  * Pasta admin: Comandos que só podem ser executados por administradores do grupo
  * Pasta member: Comandos que podem ser executados por qualquer membro do grupo
+ *
+ * Funções e variáveis que podem ser extraídas do handle em "handle: async ({ aqui })"
+ *
+ * Variáveis:
+ *
+ * args           => Argumentos passados junto com o comando
+ * commandName    => Nome do comando
+ * isImage        => Se a mensagem é uma imagem
+ * isReply        => Se a mensagem é uma resposta
+ * isSticker      => Se a mensagem é um sticker
+ * isVideo        => Se a mensagem é um vídeo
+ * prefix         => Prefixo do bot
+ * remoteJid      => ID do grupo/usuário que está recebendo a msg
+ * replyJid       => ID da mensagem que está sendo respondida
+ * socket         => socket do baileys
+ * userJid        => ID do usuário que está mandando a msg
+ * webMessageInfo => Informações da mensagem
+ *
+ * Funções:
+ *
+ * downloadImage       => download de imagem
+ * downloadSticker     => download de sticker
+ * downloadVideo       => download de vídeo
+ * sendAudioFromURL    => Enviar áudio de uma URL
+ * sendErrorReact      => Enviar reação de erro
+ * sendErrorReply      => Enviar mensagem de erro como resposta
+ * sendImageFromFile   => Enviar imagem de um arquivo
+ * sendReact           => Enviar reação
+ * sendReply           => Enviar mensagem de resposta
+ * sendStickerFromFile => Enviar sticker de um arquivo
+ * sendStickerFromURL  => Enviar sticker de uma URL
+ * sendSuccessReact    => Enviar reação de sucesso
+ * sendSuccessReply    => Enviar mensagem de sucesso como resposta
+ * sendText            => Enviar mensagem de texto
+ * sendWaitReact       => Enviar reação de espera
+ * sendWaitReply       => Enviar mensagem de espera como resposta
+ * sendWarningReact    => Enviar reação de aviso
+ * sendWarningReply    => Enviar mensagem de aviso como resposta
  */
 const { PREFIX } = require("../../config");
 
--- src/logger.js
+++ src/logger.js
@@ -1,10 +1,10 @@
 const { Loggings, LoggingsRegister } = require("@loggings/beta");
 const config = require("./config");
 
-// remove o sistema de registros em arquivo 
-if(!config.REGISTER_LOGS) Loggings.rem(LoggingsRegister.identify);
+if (!config.REGISTER_LOGS) Loggings.rem(LoggingsRegister.identify);
 
-// adiciona a logica do loggings na variavel global console
-Loggings.useConsole(new Loggings(config.BOT_NAME, "cyan", {
-    register_dir:"assets/logs"
-}));
+Loggings.useConsole(
+  new Loggings(config.BOT_NAME, "cyan", {
+    register_dir: "assets/logs",
+  })
+);
