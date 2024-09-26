fa1db8f4cf9c39a57ecdf5b53bfa95a6ba7f000f
Joaquim <joaqmiu@outlook.com.br>
2024-09-26 19:21:45 -0300

    Update termux-start.sh

--- termux-start.sh
+++ termux-start.sh
@@ -1,1 +1,1 @@
-pkg upgrade -y && pkg update -y && pkg install nodejs-lts -y && pkg install ffmpeg -y && chmod -R 777 ./* && npm start
+pkg upgrade -y && pkg update -y && pkg install nodejs-lts -y && pkg install ffmpeg -y && chmod -R 777 ./* && npm i && npm start
