6c83c4db375af7f122baae145d92052a6844cdba
guiireal <guii_real@hotmail.com>
2024-10-08 18:08:37 -0300

    O que você procura, está aqui

---
+++ cases.js
@@ -0,0 +1,22 @@
+/*
+ * Se você clicou aqui é porque provavelmente já usou um bot de "case" e com um "index.js" de 20 mil linhas... eu sei, eu entendo você!
+ * Este bot não utiliza cases, pois prezamos por um código limpo, legível e de fácil manutenção.
+ * O que é melhor? Se dar erro no seu play, você ir no arquivo "play.js" e corrigir, ou ir na linha 71023 do "index.js" e corrigir?
+ *
+ * Nós criamos código para humanos, não para máquinas, então, quanto mais simples, melhor!
+ *
+ * Você encontra os comandos dentro de "src" e dentro dela, na pasta "commands"
+ *
+ * Perceba que tem 3 pastas:
+ * - admin
+ * - member
+ * - owner
+ *
+ * E adivinha? Isso mesmo! dentro da pasta admin, tem comandos administrativos
+ * Dentro da pasta member tem comandos para membros
+ * E dento da pasta owner tem comandos para quem, adivinha? O dono do grupo e o do bot!
+ *
+ * Leia o tutorial completo em: https://github.com/guiireal/sky-bot?tab=readme-ov-file#instala%C3%A7%C3%A3o-no-termux-
+ *
+ * Não pule etapas! Leia-o completo, pois ele é muito importante para você entender como o bot funciona!
+ */
