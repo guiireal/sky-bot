d16b56f6295ea2ee41f4e9b661d4169b856c402c
guiireal <guii_real@hotmail.com>
2024-10-18 01:43:16 -0300

    wip

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,27 +1,27 @@
 const { version } = require("../../package.json");
 
 exports.sayLog = (message) => {
-  console.log("\x1b[36m[SKY BOT | 💬 ]\x1b[0m", message);
+  console.log("\x1b[36m[TAKESHI BOT | 💬 ]\x1b[0m", message);
 };
 
 exports.inputLog = (message) => {
-  console.log("\x1b[30m[SKY BOT | 📝 ]\x1b[0m", message);
+  console.log("\x1b[30m[TAKESHI BOT | 📝 ]\x1b[0m", message);
 };
 
 exports.infoLog = (message) => {
-  console.log("\x1b[34m[SKY BOT | ⏩ ]\x1b[0m", message);
+  console.log("\x1b[34m[TAKESHI BOT | ⏩ ]\x1b[0m", message);
 };
 
 exports.successLog = (message) => {
-  console.log("\x1b[32m[SKY BOT | ✅ ]\x1b[0m", message);
+  console.log("\x1b[32m[TAKESHI BOT | ✅ ]\x1b[0m", message);
 };
 
 exports.errorLog = (message) => {
-  console.log("\x1b[31m[SKY BOT | ❌ ]\x1b[0m", message);
+  console.log("\x1b[31m[TAKESHI BOT | ❌ ]\x1b[0m", message);
 };
 
 exports.warningLog = (message) => {
-  console.log("\x1b[33m[SKY BOT | ☢️  ]\x1b[0m", message);
+  console.log("\x1b[33m[TAKESHI BOT | ☢️  ]\x1b[0m", message);
 };
 
 exports.bannerLog = () => {
