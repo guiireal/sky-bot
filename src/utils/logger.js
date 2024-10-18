fb197348a46d5507e85cfe10e13cabef08fc6b07
guiireal <guii_real@hotmail.com>
2024-10-18 01:47:57 -0300

    wip

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,27 +1,27 @@
 const { version } = require("../../package.json");
 
 exports.sayLog = (message) => {
-  console.log("\x1b[36m[TAKESHI BOT | 💬 ]\x1b[0m", message);
+  console.log("\x1b[36m[TAKESHI BOT | TALK]\x1b[0m", message);
 };
 
 exports.inputLog = (message) => {
-  console.log("\x1b[30m[TAKESHI BOT | 📝 ]\x1b[0m", message);
+  console.log("\x1b[30m[TAKESHI BOT | INPUT]\x1b[0m", message);
 };
 
 exports.infoLog = (message) => {
-  console.log("\x1b[34m[TAKESHI BOT |  ℹ ]\x1b[0m", message);
+  console.log("\x1b[34m[TAKESHI BOT |  INFO]\x1b[0m", message);
 };
 
 exports.successLog = (message) => {
-  console.log("\x1b[32m[TAKESHI BOT | ✅ ]\x1b[0m", message);
+  console.log("\x1b[32m[TAKESHI BOT | SUCCESS]\x1b[0m", message);
 };
 
 exports.errorLog = (message) => {
-  console.log("\x1b[31m[TAKESHI BOT | ❌ ]\x1b[0m", message);
+  console.log("\x1b[31m[TAKESHI BOT | ERROR]\x1b[0m", message);
 };
 
 exports.warningLog = (message) => {
-  console.log("\x1b[33m[TAKESHI BOT | ☢️  ]\x1b[0m", message);
+  console.log("\x1b[33m[TAKESHI BOT | WARNING]\x1b[0m", message);
 };
 
 exports.bannerLog = () => {
